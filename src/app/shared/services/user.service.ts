17415083e140236a87ecf398876a4a647cf3ecf3
Rohan Prabhune <rohanprabhune@benchmarkit.solutions>
2020-07-23 21:04:51 +0530

    folder structure changed

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpReqMethod, ApiService } from '../services/api.service';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
