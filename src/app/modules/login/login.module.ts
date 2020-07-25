f1dbce95ab507adb1e9f5366cbda096f101ffb10
pranay <pranaypalaskar@benchmarkit.solutions>
2020-07-25 18:04:20 +0530

    changes related to login

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { SharedModule } from '../../shared/shared.module';
@@ -9,10 +7,7 @@ import { SharedModule } from '../../shared/shared.module';
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule,
     LoginRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     SharedModule
   ]
 })
