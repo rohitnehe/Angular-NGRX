8f65fc26be8bd8cdeb55aed2eb076717bc2744f7
Rohan Prabhune <rohanprabhune@benchmarkit.solutions>
2020-07-27 20:05:14 +0530

    chnages

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpReqMethod, ApiService } from '../services/api.service';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs/Observable';
+import { ValidationMessage } from '../models/validation-message.model';
+
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +15,11 @@ export class UserService {
   constructor(private apiService: ApiService) { }
 
   // validation messages for create user
-  signupValidationMessage() {
+  signupValidationMessage(): Observable<ValidationMessage> {
     return this.apiService.Request(environment.serviceUrl + 'validationMessage?key=create-account', HttpReqMethod.GET.toString());
   }
 
-  loginValidationMessage() {
+  loginValidationMessage(): Observable<ValidationMessage> {
     return this.apiService.Request(environment.serviceUrl + 'validationMessage?key=login', HttpReqMethod.GET.toString());
   }
 
