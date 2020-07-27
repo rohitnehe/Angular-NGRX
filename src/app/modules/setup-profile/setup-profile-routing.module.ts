b9dad828ec5250e14931077867124209ef982909
Nitin patil <nitinp@bitsdev.in>
2020-07-27 20:14:49 +0530

    changes

--- src/app/modules/setup-profile/setup-profile-routing.module.ts
+++ src/app/modules/setup-profile/setup-profile-routing.module.ts
@@ -1,10 +1,11 @@
-import { ProfileSetupCompleteComponent } from './profile-setup-complete/profile-setup-complete.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SecurityComponent } from './security/security.component';
-import { AddressComponent } from './address/address.component';
-import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
-import { BioDetailsComponent } from './bio-details/bio-details.component';
+import { SecurityComponent } from './components/security/security.component';
+import { AddressComponent } from './components/address/address.component';
+import { ProfilePictureComponent } from './components/profile-picture/profile-picture.component';
+import { BioDetailsComponent } from './components/bio-details/bio-details.component';
+import { ProfileSetupCompleteComponent } from './components/profile-setup-complete/profile-setup-complete.component';
 import { AuthGuardService as AuthGuard} from '../../helper/auth/auth.guard.service';
 
 
--- src/app/modules/setup-profile/setup-profile.module.ts
+++ src/app/modules/setup-profile/setup-profile.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SetupProfileRoutingModule } from './setup-profile-routing.module';
-import { BioDetailsComponent } from './bio-details/bio-details.component';
-import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
-import { AddressComponent } from './address/address.component';
-import { SecurityComponent } from './security/security.component';
+import { BioDetailsComponent } from './components/bio-details/bio-details.component';
+import { ProfilePictureComponent } from './components/profile-picture/profile-picture.component';
+import { AddressComponent } from './components/address/address.component';
+import { SecurityComponent } from './components/security/security.component';
 import { SharedModule } from '../../shared/shared.module';
-import { ProfileSetupCompleteComponent } from './profile-setup-complete/profile-setup-complete.component';
+import { ProfileSetupCompleteComponent } from './components/profile-setup-complete/profile-setup-complete.component';
 
 
 @NgModule({
