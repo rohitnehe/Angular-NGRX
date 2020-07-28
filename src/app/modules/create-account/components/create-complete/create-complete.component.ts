32af214b340130bd29edd6481feafa1bac8a950a
Rohan Prabhune <rohanprabhune@benchmarkit.solutions>
2020-07-28 20:06:28 +0530

    create accout complete

--- src/app/modules/create-account/components/create-complete/create-complete.component.ts
+++ src/app/modules/create-account/components/create-complete/create-complete.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { LogOut } from '../../../../store/actions/auth.actions';
 import { AppState} from '../../../../store/app.states';
+import { PageDataService } from '../../../../services/page.data.service';
+import { ErrorHandler } from '../../../../helpers/error-handler';
 
 @Component({
   selector: 'app-create-complete',
@@ -10,16 +12,31 @@ import { AppState} from '../../../../store/app.states';
 })
 export class CreateCompleteComponent implements OnInit {
 
+  title: string;
+  message: string;
+  error: any = {};
+
   constructor(
     private store: Store<AppState>,
+    private pageDataService: PageDataService,
+    private errorHandler: ErrorHandler,
   ) {}
 
   ngOnInit(): void {
+    this.accountCreatedMessage();
   }
 
    // logout user
    logOut(): void {
     this.store.dispatch(new LogOut());
+  }
+
+  // successful account creation message
+  accountCreatedMessage(){
+    this.pageDataService.getAccountCreationMessage().subscribe(response => {
+      this.title = response[0].title;
+      this.message = response[0].content;
+    }, (error) => { this.error = this.errorHandler.errorCallback(error); });
   }
 
 }
